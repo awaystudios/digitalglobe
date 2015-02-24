ce0ac91713cd3ee68f2ba775d8e2078b91cc85f0
James Rosewell <support@51degrees.com>
2015-02-24 10:01:23 +0000

    2 Globes
    Added iFrame.html to display two globes next to each other for the stand
    presentation.

--- Chrome.bat
+++ Chrome.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" --allow-file-access-from-files "C:\Users\James\Documents\GitHub\digitalglobe\index.html"
+"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" --allow-file-access-from-files "C:\Users\James\Documents\GitHub\digitalglobe\iframe.html"
